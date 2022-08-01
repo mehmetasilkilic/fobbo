0349de4b311b96d9340b3bce174d0e89d25c378c
mehmetasilkilic <mehmetasil96@gmail.com>
2022-08-01 11:09:55 +0300

    N: infrastructure created

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components";
+
+import { useFonts as useOswald, Oswald_400Regular } from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+import { theme } from "./src/infrastructure/theme";
 import { PlacesScreen } from "./src/features/place/screens/places.screen";
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({ Oswald_400Regular });
+  const [latoLoaded] = useLato({ Lato_400Regular });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
-      <PlacesScreen />
-      <ExpoStatusBar style="auto" />
+      <ThemeProvider theme={theme}>
+        <PlacesScreen />
+        <ExpoStatusBar style="auto" />
+      </ThemeProvider>
     </>
   );
 }

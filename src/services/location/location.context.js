1f8d7d7ae406ad091b452d4fb321e39c59da554a
mehmetasilkilic <mehmetasil96@gmail.com>
2022-08-11 12:37:32 +0300

    N: location services added

---
+++ src/services/location/location.context.js
@@ -0,0 +1,44 @@
+import { useState, useEffect, createContext } from "react";
+
+import { locationRequest, locationTransform } from "./location.service";
+
+export const LocationContext = createContext();
+
+export const LocationContextProvider = ({ children }) => {
+  const [keyword, setKeyword] = useState("San Francisco");
+  const [location, setLocation] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const onSearch = (searchKeyword) => {
+    setIsLoading(true);
+    setKeyword(searchKeyword);
+    if (!searchKeyword.length) {
+      return;
+    }
+    locationRequest(searchKeyword.toLowerCase())
+      .then(locationTransform)
+      .then((res) => {
+        setLocation(res);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <LocationContext.Provider
+      value={{
+        isLoading,
+        error,
+        location,
+        search: onSearch,
+        keyword,
+      }}
+    >
+      {children}
+    </LocationContext.Provider>
+  );
+};

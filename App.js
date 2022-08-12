4416400eabfe29b3c5518da4eb2fe7423cba4ef0
mehmetasilkilic <mehmetasil96@gmail.com>
2022-08-12 15:10:02 +0300

    U: navigation restructure

--- App.js
+++ App.js
@@ -1,8 +1,5 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Ionicons } from "@expo/vector-icons";
 
 import {
   useFonts as useOswald,
@@ -11,44 +8,10 @@ import {
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme";
-import { PlacesScreen } from "./src/features/place/screens/places.screen";
+import { Navigation } from "./src/infrastructure/navigation";
 
 import { PlacesContextProvider } from "./src/services/places/places.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
-
-import { SafeArea } from "./src/components/utils/safeArea.component";
-import { Text } from "react-native";
-
-const Tab = createBottomTabNavigator();
-
-const SettingsScreen = () => (
-  <SafeArea>
-    <Text>Settings</Text>
-  </SafeArea>
-);
-
-const MapScreen = () => (
-  <SafeArea>
-    <Text>Map</Text>
-  </SafeArea>
-);
-
-const TAB_ICON = {
-  Places: "md-restaurant",
-  Map: "md-map",
-  Settings: "md-settings",
-};
-
-const screenOptions = ({ route }) => {
-  const iconName = TAB_ICON[route.name];
-  return {
-    tabBarIcon: ({ size, color }) => (
-      <Ionicons name={iconName} size={size} color={color} />
-    ),
-    tabBarActiveTintColor: "tomato",
-    tabBarInactiveTintColor: "gray",
-  };
-};
 
 export default function App() {
   const [oswaldLoaded] = useOswald({ Oswald_400Regular });
@@ -63,13 +26,7 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <LocationContextProvider>
           <PlacesContextProvider>
-            <NavigationContainer>
-              <Tab.Navigator screenOptions={screenOptions}>
-                <Tab.Screen name="Places" component={PlacesScreen} />
-                <Tab.Screen name="Map" component={MapScreen} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
-              </Tab.Navigator>
-            </NavigationContainer>
+            <Navigation />
           </PlacesContextProvider>
         </LocationContextProvider>
       </ThemeProvider>
---
+++ src/infrastructure/navigation/app.navigator.js
@@ -0,0 +1,47 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { Text } from "react-native";
+
+import { PlacesScreen } from "../../features/place/screens/places.screen";
+import { SafeArea } from "../../components/utils/safeArea.component";
+
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Places: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
+
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
+const Map = () => (
+  <SafeArea>
+    <Text>Map</Text>
+  </SafeArea>
+);
+
+const screenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: "tomato",
+    tabBarInactiveTintColor: "gray",
+  };
+};
+
+export const AppNavigator = () => (
+  <NavigationContainer>
+    <Tab.Navigator screenOptions={screenOptions}>
+      <Tab.Screen name="Places" component={PlacesScreen} />
+      <Tab.Screen name="Map" component={Map} />
+      <Tab.Screen name="Settings" component={Settings} />
+    </Tab.Navigator>
+  </NavigationContainer>
+);

364c9006faafc7c561dd9784f674fdea7484ae93
mehmetasilkilic <mehmetasil96@gmail.com>
2022-08-14 12:13:13 +0300

    N: placeDetail topBar added

--- src/features/place/screens/placeDetail.screen.js
+++ src/features/place/screens/placeDetail.screen.js
@@ -1,12 +1,16 @@
+import { View, ScrollView } from "react-native";
+import { SliderBox } from "react-native-image-slider-box";
+import { Appbar } from "react-native-paper";
+
 import { SvgXml } from "react-native-svg";
 
-import { SliderBox } from "react-native-image-slider-box";
-
 import { SafeArea } from "../../../components/utils/safeArea.component";
-
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { Text } from "../../../components/typography/text.component";
+
 import star from "../../../../assets/star";
+import success from "../../../../assets/success";
+import comment from "../../../../assets/comment";
 
 import {
   HeaderContainer,
@@ -16,10 +20,24 @@ import {
   RatingContainerTop,
   RatingContainerBottom,
   Types,
+  IfOpen,
 } from "./placeDetail.styles";
 
 export const PlaceDetailScreen = ({ route }) => {
   const { place } = route.params;
+
+  const appBarStyle = {
+    paddingRight: 8,
+    paddingLeft: 0,
+    marginTop: 0,
+    backgroundColor: "#9C1F19",
+    justifyContent: "space-between",
+  };
+
+  const appBarIconStyle = {
+    padding: 0,
+    margin: 0,
+  };
 
   const dotStyle = {
     width: 40,
@@ -30,44 +48,77 @@ export const PlaceDetailScreen = ({ route }) => {
   };
 
   const typeCount = place.types.length > 3 ? 3 : place.types.length;
-
   return (
     <SafeArea>
-      <HeaderContainer>
-        <Spacer position="bottom" size="large">
-          <Row>
-            <Title variant="title">{place.name}</Title>
-            <RatingContainer>
-              <RatingContainerTop>
-                <SvgXml xml={star} width={18} height={18} />
-                <Text variant="error">{place.rating} / 5</Text>
-              </RatingContainerTop>
-              <RatingContainerBottom>
-                <SvgXml xml={star} width={18} height={18} />
-                <Text variant="error">200+</Text>
-              </RatingContainerBottom>
-            </RatingContainer>
-          </Row>
-        </Spacer>
-        <Spacer position="bottom" size="small">
-          <Types>
-            {place.types.slice(0, typeCount).map((item, index) => (
-              <Text variant="subTitle">
-                {item.charAt(0).toUpperCase() + item.slice(1)}
-                {index < typeCount - 1 ? ", " : ""}
-              </Text>
-            ))}
-          </Types>
-        </Spacer>
-        <Text variant="lightError">{place.address}</Text>
-      </HeaderContainer>
-      <SliderBox
-        images={place.photos}
-        dotColor="#9C1F19"
-        inactiveDotColor="#FDDEBB"
-        dotStyle={dotStyle}
-        circleLoop
-      />
+      <Appbar.Header style={appBarStyle}>
+        <View>
+          <Appbar.Action
+            color="white"
+            icon="chevron-down"
+            size={30}
+            onPress={() => {}}
+          />
+        </View>
+        <Types>
+          <Appbar.Action
+            style={appBarIconStyle}
+            color="white"
+            icon="map-marker-outline"
+            size={26}
+            onPress={() => {}}
+          />
+          <Appbar.Action
+            style={appBarIconStyle}
+            color="white"
+            icon="cards-heart-outline"
+            size={26}
+            onPress={() => {}}
+          />
+        </Types>
+      </Appbar.Header>
+      <ScrollView>
+        <HeaderContainer>
+          <Spacer position="bottom" size="large">
+            <Row>
+              <Title variant="title">{place.name}</Title>
+              <RatingContainer>
+                <RatingContainerTop>
+                  <SvgXml xml={star} width={18} height={18} />
+                  <Text variant="error">{place.rating} / 5</Text>
+                </RatingContainerTop>
+                <RatingContainerBottom>
+                  <SvgXml xml={comment} width={18} height={18} />
+                  <Text variant="error">200+</Text>
+                </RatingContainerBottom>
+              </RatingContainer>
+            </Row>
+          </Spacer>
+          <Spacer position="bottom" size="small">
+            <Types>
+              {place.types.slice(0, typeCount).map((item, index) => (
+                <Text key={index} variant="subTitle">
+                  {item.charAt(0).toUpperCase() + item.slice(1)}
+                  {index < typeCount - 1 ? ", " : ""}
+                </Text>
+              ))}
+            </Types>
+          </Spacer>
+          <Spacer position="bottom" size="small">
+            <Text variant="lightError">{place.address}</Text>
+          </Spacer>
+          <IfOpen>
+            <SvgXml xml={success} width={18} height={18} />
+            <Text variant="success">Open now</Text>
+          </IfOpen>
+        </HeaderContainer>
+        <SliderBox
+          images={place.photos}
+          dotColor="#9C1F19"
+          inactiveDotColor="#FDDEBB"
+          dotStyle={dotStyle}
+          circleLoop
+        />
+      </ScrollView>
     </SafeArea>
   );
 };
--- src/features/place/screens/placeDetail.styles.js
+++ src/features/place/screens/placeDetail.styles.js
@@ -3,7 +3,9 @@ import { Text } from "../../../components/typography/text.component";
 
 export const HeaderContainer = styled.View`
   min-height: 100px;
-  padding: ${(props) => props.theme.space[3]};
+  padding-right: ${(props) => props.theme.space[3]};
+  padding-bottom: ${(props) => props.theme.space[3]};
+  padding-left: ${(props) => props.theme.space[3]};
   background-color: ${(props) => props.theme.colors.brand.primary};
 `;
 
@@ -31,9 +33,8 @@ export const RatingContainerTop = styled.View`
   justify-content: space-between;
   align-items: center;
   background-color: ${(props) => props.theme.colors.brand.secondary};
-  width: 60px;
+  width: 65px;
   border-top-left-radius: 5px;
-  border-top-right-radius: 5px;
   padding: ${(props) => props.theme.space[1]};
 `;
 
@@ -42,8 +43,19 @@ export const RatingContainerBottom = styled.View`
   justify-content: space-between;
   align-items: center;
   background-color: ${(props) => props.theme.colors.ui.quaternary};
-  width: 60px;
-  border-bottom-left-radius: 5px;
+  width: 65px;
   border-bottom-right-radius: 5px;
   padding: ${(props) => props.theme.space[1]};
+`;
+
+export const IfOpen = styled.View`
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: ${(props) => props.theme.colors.ui.disabled};
+  color: ${(props) => props.theme.colors.ui.success};
+  width: 90px;
+  height: 25px;
+  border-bottom-right-radius: 5px;
+  border-top-left-radius: 5px;
 `;

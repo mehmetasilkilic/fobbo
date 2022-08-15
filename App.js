3201dcaa19d7d753200ababd09a73436dfea5777
mehmetasilkilic <mehmetasil96@gmail.com>
2022-08-15 16:39:53 +0300

    N: favourites context added

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import { Navigation } from "./src/infrastructure/navigation";
 
 import { PlacesContextProvider } from "./src/services/places/places.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
+import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 
 export default function App() {
   const [oswaldLoaded] = useOswald({ Oswald_400Regular });
@@ -24,11 +25,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <LocationContextProvider>
-          <PlacesContextProvider>
-            <Navigation />
-          </PlacesContextProvider>
-        </LocationContextProvider>
+        <FavouritesContextProvider>
+          <LocationContextProvider>
+            <PlacesContextProvider>
+              <Navigation />
+            </PlacesContextProvider>
+          </LocationContextProvider>
+        </FavouritesContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>

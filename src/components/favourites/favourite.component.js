184b38e2770a26e868801d749ebdf6adf62de138
mehmetasilkilic <mehmetasil96@gmail.com>
2022-08-16 12:56:26 +0300

    N: favourite icon component created

---
+++ src/components/favourites/favourite.component.js
@@ -0,0 +1,38 @@
+import { useContext } from "react";
+import { TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+import { AntDesign } from "@expo/vector-icons";
+
+import { FavouritesContext } from "../../services/favourites/favourites.context";
+
+const CardStyle = styled(TouchableOpacity)`
+  position: absolute;
+  top: ${(props) => props.theme.space[3]};
+  right: ${(props) => props.theme.space[3]};
+  z-index: 9;
+`;
+
+const DetailStyle = styled(TouchableOpacity)``;
+
+export const Favourite = ({ place, variable }) => {
+  const FavouriteButton = variable === 1 ? CardStyle : DetailStyle;
+
+  const { favourites, addToFavourites, removeFromFavourites } =
+    useContext(FavouritesContext);
+
+  const isFavourite = favourites.find((item) => item.placeId === place.placeId);
+
+  return (
+    <FavouriteButton
+      onPress={() =>
+        !isFavourite ? addToFavourites(place) : removeFromFavourites(place)
+      }
+    >
+      <AntDesign
+        name={isFavourite ? "heart" : "hearto"}
+        size={24}
+        color={isFavourite ? "white" : "white"}
+      />
+    </FavouriteButton>
+  );
+};

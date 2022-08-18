2b385a5c7ff50e8d8ffb71c1e5420bf937a2b5dd
mehmetasilkilic <mehmetasil96@gmail.com>
2022-08-18 12:51:06 +0300

    N: title added to horizontalPlaceList

--- src/features/home/components/horizontalPlaceList.component.js
+++ src/features/home/components/horizontalPlaceList.component.js
@@ -1,15 +1,21 @@
 import { FlatList, TouchableOpacity } from "react-native";
-import styled from "styled-components/native";
+import { AntDesign } from "@expo/vector-icons";
 
 import { PlaceInfoCard } from "../../place/components/placeInfoCard.components";
 import { Spacer } from "../../../components/spacer/spacer.component";
+import { Text } from "../../../components/typography/text.component";
 
-const HorizontalBarContainer = styled.View`
-  padding: 10px;
-`;
+import { Row, InlineRow } from "./horizontalPlaceList.styles";
 
 export const HorizontalPlaceList = ({ data, onNavigate }) => (
-  <HorizontalBarContainer>
+  <>
+    <Row>
+      <Text variant="boldLabel">Trending this week</Text>
+      <InlineRow>
+        <Text variant="error">View all </Text>
+        <AntDesign name="doubleright" size={10} color="#D0421B" />
+      </InlineRow>
+    </Row>
     <FlatList
       data={data}
       horizontal
@@ -32,5 +38,5 @@ export const HorizontalPlaceList = ({ data, onNavigate }) => (
       }}
       keyExtractor={(item) => item.name.split(" ").join("")}
     />
-  </HorizontalBarContainer>
+  </>
 );
---
+++ src/features/home/components/horizontalPlaceList.styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Row = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border-top-width: 1px;
+  border-top-color: ${(props) => props.theme.colors.ui.disabled};
+  padding: ${(props) => props.theme.space[2]};
+`;
+
+export const InlineRow = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;

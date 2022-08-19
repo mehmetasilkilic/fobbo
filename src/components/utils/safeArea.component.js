9b0ef5c3bb86b5920e94c41fe1c58eb20ca0b3ff
mehmetasilkilic <mehmetasil96@gmail.com>
2022-08-19 20:23:19 +0300

    U: background color changed

--- src/components/utils/safeArea.component.js
+++ src/components/utils/safeArea.component.js
@@ -3,5 +3,7 @@ import { StatusBar, SafeAreaView } from "react-native";
 
 export const SafeArea = styled(SafeAreaView)`
   flex: 1;
+  background-color: ${(props) => props.theme.colors.ui.tertiary};
   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`}
+  margin-bottom: 80px;
 `;

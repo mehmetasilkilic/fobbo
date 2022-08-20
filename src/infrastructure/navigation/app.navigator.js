221a02daa9479666a365df58275c3377be528cbd
mehmetasilkilic <mehmetasil96@gmail.com>
2022-08-20 19:09:18 +0300

    B: tabbar does not hidden bug fixed

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import { Image } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  getFocusedRouteNameFromRoute,
+} from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { AntDesign } from "@expo/vector-icons";
 
@@ -11,6 +15,19 @@ import { ProfileNavigator } from "./profile.navigator";
 
 import navLogo from "../../../assets/nav-logo.png";
 
+let placesTab = "";
+let profileTab = "";
+
+const getPlacesHeaderTitle = (route) => {
+  const routeName = getFocusedRouteNameFromRoute(route) ?? "Places";
+  placesTab = routeName;
+};
+
+const getProfileHeaderTitle = (route) => {
+  const routeName = getFocusedRouteNameFromRoute(route) ?? "Profile";
+  profileTab = routeName;
+};
+
 const Tab = createBottomTabNavigator();
 
 const TAB_ICON = {
@@ -18,19 +35,6 @@ const TAB_ICON = {
   Places: "appstore-o",
   Map: "enviromento",
   Profile: "user",
-};
-
-const tabBarStyle = {
-  position: "absolute",
-  margin: 10,
-  borderRadius: 5,
-  height: 70,
-  shadowColor: "#000000",
-  shadowOpacity: 0.06,
-  shadowOffset: {
-    width: 10,
-    height: 10,
-  },
 };
 
 const tabBarStyleHidden = {
@@ -53,9 +57,10 @@ const tabBarActionButtonStyle = {
   },
 };
 
+let tabBarStyle = {};
+
 const screenOptions = ({ route }) => {
   const iconName = TAB_ICON[route.name];
-
   return {
     tabBarIcon:
       route.name !== "Action"
@@ -71,14 +76,52 @@ const screenOptions = ({ route }) => {
   };
 };
 
-export const AppNavigator = () => (
-  <NavigationContainer>
-    <Tab.Navigator screenOptions={screenOptions}>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Places" component={PlacesNavigator} />
-      <Tab.Screen name="Action" component={Action} />
-      <Tab.Screen name="Map" component={MapScreen} />
-      <Tab.Screen name="Profile" component={ProfileNavigator} />
-    </Tab.Navigator>
-  </NavigationContainer>
-);
+export const AppNavigator = () => {
+  const [displayMode, setDisplayMode] = useState("flex");
+
+  tabBarStyle = {
+    display: displayMode,
+    position: "absolute",
+    margin: 10,
+    borderRadius: 5,
+    height: 70,
+    shadowColor: "#000000",
+    shadowOpacity: 0.06,
+    shadowOffset: {
+      width: 10,
+      height: 10,
+    },
+  };
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen
+          name="Places"
+          component={PlacesNavigator}
+          options={({ route }) => {
+            getPlacesHeaderTitle(route);
+            placesTab === "PlaceDetail"
+              ? setDisplayMode("none")
+              : setDisplayMode("flex");
+          }}
+        />
+        <Tab.Screen name="Action" component={Action} />
+        <Tab.Screen name="Map" component={MapScreen} />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileNavigator}
+          options={({ route }) => {
+            getProfileHeaderTitle(route);
+            profileTab === "Favourites"
+              ? setDisplayMode("none")
+              : placesTab === "PlaceDetail"
+              ? setDisplayMode("none")
+              : setDisplayMode("flex");
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};

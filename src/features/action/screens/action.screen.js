231f14603bdc4ce6f5fb72db807d695bf301496a
mehmetasilkilic <mehmetasil96@gmail.com>
2022-08-23 11:31:45 +0300

    N: image slider added to action screen

--- src/features/action/screens/action.screen.js
+++ src/features/action/screens/action.screen.js
@@ -1,11 +1,43 @@
+import { TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
 import { SafeArea } from "../../../components/utils/safeArea.component";
 import { Search } from "../../../components/search/search.component";
+import { ImageSlider } from "../../../components/imageSlider/imageSlider.component";
+import { Text } from "../../../components/typography/text.component";
 
 import { Buttons } from "../components/buttons.component";
 
+import { SliderContainer, TopBar, Row, ActionContainer } from "./action.styles";
+
+const data = [
+  "https://images.unsplash.com/photo-1516382564206-12736c7da10b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  "https://images.unsplash.com/photo-1481833761820-0509d3217039?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  "https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+  "https://images.unsplash.com/photo-1551632436-cbf8dd35adfa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+  "https://images.unsplash.com/photo-1559339352-11d035aa65de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+  "https://images.unsplash.com/photo-1575444758702-4a6b9222336e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+];
+
 export const Action = () => (
   <SafeArea>
-    <Search />
-    <Buttons />
+    <ActionContainer>
+      <TopBar>
+        <Row>
+          <TouchableOpacity>
+            <AntDesign name="enviromento" size={24} color={"white"} />
+          </TouchableOpacity>
+          <Text variant="whiteButton">Ataköy 9. Kısım, Uğur Mumcu Bulvarı</Text>
+        </Row>
+        <TouchableOpacity>
+          <AntDesign name="filter" size={24} color={"white"} />
+        </TouchableOpacity>
+      </TopBar>
+      <Search />
+      <SliderContainer>
+        <ImageSlider images={data} />
+      </SliderContainer>
+      <Buttons />
+    </ActionContainer>
   </SafeArea>
 );
--- src/features/action/screens/action.styles.js
+++ src/features/action/screens/action.styles.js
@@ -1,9 +1,26 @@
 import styled from "styled-components/native";
 
+export const ActionContainer = styled.View`
+  flex: 1;
+  background-color: ${(props) => props.theme.colors.ui.quaternary};
+`;
+
+export const TopBar = styled.View`
+  padding: ${(props) => props.theme.space[2]};
+  flex-direction: row;
+  background-color: ${(props) => props.theme.colors.brand.primary};
+  justify-content: space-between;
+  align-items: center;
+`;
+
 export const Row = styled.View`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  padding-horizontal: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+export const SliderContainer = styled.View`
+  margin: ${(props) => props.theme.space[2]};
+  border-radius: 5px;
+  overflow: hidden;
 `;

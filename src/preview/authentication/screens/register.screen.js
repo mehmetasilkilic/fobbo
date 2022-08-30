13d2acfdd8d67d5f187aa19cec8834da94b131dd
mehmetasilkilic <mehmetasil96@gmail.com>
2022-08-30 17:52:40 +0300

    N: error toast added to register

--- src/preview/authentication/screens/register.screen.js
+++ src/preview/authentication/screens/register.screen.js
@@ -1,4 +1,8 @@
 import { View } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import Toast from "react-native-toast-message";
+
+import { removeError } from "../../../store/user/user.slice";
 
 import { Text } from "../../../components/typography/text.component";
 import { SafeArea } from "../../../components/utils/safeArea.component";
@@ -7,18 +11,32 @@ import { AuthOptions } from "../components/authOptions.component";
 import { Agreements } from "../components/agreements.component";
 import { RegisterForm } from "../components/registerForm.component";
 
+import { errorToastConfig } from "../../../utils/errorToastConfig";
+
 import { RegisterContainer, MidRow, OrContainer } from "./register.styles";
 
 export const Register = ({ navigation }) => {
+  const dispatch = useDispatch();
+
   const nav = () => navigation.navigate("Onboarding");
   const goLogin = () => navigation.navigate("Login");
+
+  const error = useSelector((state) => state.user.error);
+  const showToast = () => {
+    console.log("text", error);
+    Toast.show({
+      type: "error",
+      text1: error,
+    });
+    dispatch(removeError());
+  };
 
   return (
     <SafeArea>
       <RegisterContainer>
         <Text variant="titleBig">Welcome to Fobbo</Text>
         <View>
-          <RegisterForm />
+          <RegisterForm errorToast={showToast} />
           <MidRow>
             <OrContainer>
               <Text variant="whiteButton">OR</Text>
@@ -28,6 +46,7 @@ export const Register = ({ navigation }) => {
         </View>
         <Agreements />
       </RegisterContainer>
+      <Toast config={errorToastConfig} />
     </SafeArea>
   );
 };

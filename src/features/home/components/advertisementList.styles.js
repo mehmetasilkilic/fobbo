b37f2a923e7f92094d9785901a3cfbd75b8f9451
mehmetasilkilic <mehmetasil96@gmail.com>
2022-08-31 10:58:33 +0300

    Code cleanup

--- src/features/home/components/advertisementList.styles.js
+++ src/features/home/components/advertisementList.styles.js
@@ -5,8 +5,7 @@ export const AdvertisementFlatList = styled(FlatList)`
   flex-grow: 0;
   border-top-width: 1px;
   border-top-color: ${(props) => props.theme.colors.ui.disabled};
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
+  padding-vertical: ${(props) => props.theme.space[2]};
 `;
 
 export const CompactImage = styled.Image`

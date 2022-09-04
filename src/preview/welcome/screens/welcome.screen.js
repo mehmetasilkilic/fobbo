27cfd5dc5b75a84eb4c1b22bd77d2ac6246228ef
ebasturmen <emre@merzigo.com>
2022-09-04 11:49:40 +0300

    N: register as a guest added

--- src/preview/welcome/screens/welcome.screen.js
+++ src/preview/welcome/screens/welcome.screen.js
@@ -1,4 +1,8 @@
 import { TouchableOpacity } from "react-native";
+import { useDispatch } from "react-redux";
+import * as Device from "expo-device";
+
+import { register } from "../../../store/user/user.service";
 
 import { SafeArea } from "../../../components/utils/safeArea.component";
 import { Text } from "../../../components/typography/text.component";
@@ -7,31 +11,45 @@ import { Button } from "../../../components/button/button.component";
 
 import { AuthContainer, Row, WelcomeContainer } from "./welcome.styles";
 
-export const Welcome = ({ navigation }) => (
-  <SafeArea>
-    <WelcomeContainer>
-      <Text>Fobbo</Text>
-      <AuthContainer>
-        <Spacer position="bottom" size="medium">
-          <TouchableOpacity onPress={() => navigation.navigate("Onboarding")}>
-            <Text variant="brand">Continue as a guest</Text>
-          </TouchableOpacity>
-        </Spacer>
-        <Button
-          onTouch={() => navigation.navigate("Register")}
-          text="Register"
-        />
-        <Spacer position="top" size="medium">
-          <Row>
-            <Spacer position="right" size="medium">
-              <Text variant="label">Dou you have an account?</Text>
-            </Spacer>
-            <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-              <Text variant="brand">Login</Text>
+export const Welcome = ({ navigation }) => {
+  const dispatch = useDispatch();
+
+  const deviceName = `${Device.brand}-${Device.modelName}`;
+
+  const formData = {
+    device_name: deviceName,
+  };
+
+  const handleSubmit = () => {
+    dispatch(register(formData));
+  };
+
+  return (
+    <SafeArea>
+      <WelcomeContainer>
+        <Text>Fobbo</Text>
+        <AuthContainer>
+          <Spacer position="bottom" size="medium">
+            <TouchableOpacity onPress={handleSubmit}>
+              <Text variant="brand">Continue as a guest</Text>
             </TouchableOpacity>
-          </Row>
-        </Spacer>
-      </AuthContainer>
-    </WelcomeContainer>
-  </SafeArea>
-);
+          </Spacer>
+          <Button
+            onTouch={() => navigation.navigate("Register")}
+            text="Register"
+          />
+          <Spacer position="top" size="medium">
+            <Row>
+              <Spacer position="right" size="medium">
+                <Text variant="label">Dou you have an account?</Text>
+              </Spacer>
+              <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+                <Text variant="brand">Login</Text>
+              </TouchableOpacity>
+            </Row>
+          </Spacer>
+        </AuthContainer>
+      </WelcomeContainer>
+    </SafeArea>
+  );
+};
